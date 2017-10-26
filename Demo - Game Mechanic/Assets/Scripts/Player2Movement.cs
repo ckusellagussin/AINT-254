471c3e517a49b6aa875c21f222c81e32654a9e08
Casey Kusella-Gussin <casey.kusella-gussin@students.plymouth.ac.uk>
2017-10-26 08:37:57 +0100

    Multiplayer
    Added new ConeMan and functionality for it

---
+++ Demo - Game Mechanic/Assets/Scripts/Player2Movement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2Movement : MonoBehaviour
+{
+
+    public float movementSpeed = 2.00f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        ControllPlayer();
+    }
+
+
+    void ControllPlayer()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal1");
+        float moveVertical = Input.GetAxisRaw("Vertical1");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
+
+
+        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
+
+    }
+
+}
